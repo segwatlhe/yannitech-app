a76765d3a506a5ba91d6458ceb1a410ee730cbdf
segwatlhe <segwatlhe@gmail.com>
2020-10-09 08:41:10 +0200

    cleaning

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -25,7 +25,6 @@ export class BookDetailsComponent implements OnInit {
 
     this.bookService.getBook(this.id)
       .subscribe(data => {
-        console.log(data);
         this.book = data;
       }, error => {
         this.handleError(error);
